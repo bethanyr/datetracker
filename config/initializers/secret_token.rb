fdfdb7ebdb2149052d8445d0030114beffe9aeaa
bethanyr <bethany.rentz@hotmail.com>
2013-07-16 19:53:13 -0700

    initial version of datetracker

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datetracker::Application.config.secret_token = '5a535dd606758616db3c1fb9047040bb65bb282aa55e572e48904c6e8771e014cbfeab167d95814cbe416cf98854dd85b0bd66fbd595c714f685b94aefdfed92'
